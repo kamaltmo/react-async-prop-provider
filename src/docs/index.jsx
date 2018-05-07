746ee763e29387905ef40d77704b1ffcdda63a6d
Kamal Osman <kamaltmo@gmail.com>
2018-05-07 12:50:40 +0100

    AsyncPropProvider - Initial Commit

---
+++ src/docs/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+import "./styles.css";
+
+function Demo() {
+  return (
+    <div>
+      <h1>Demo with examples of the component</h1>
+    </div>
+  );
+}
+
+render(<Demo />, document.getElementById("app"));
---
+++ src/lib/index.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class AsyncPropProvider extends Component {
+  state = {
+    promiseState: null
+  };
+
+  actionHandler = (...args) => {
+    const promise = this.props.action(...args);
+    if (promise && typeof promise.then === "function") {
+      this.setState({
+        promiseState: "pending"
+      });
+      promise
+        .then(() => {
+          this.setState({
+            promiseState: "fulfilled"
+          });
+        })
+        .catch(error => {
+          this.setState({
+            promiseState: "rejected"
+          });
+          throw error;
+        });
+    }
+  };
+
+  asyncProps = () => {
+    const {
+      pendingProps = {},
+      rejectedProps = {},
+      fulfilledProps = {}
+    } = this.props;
+    switch (this.state.promiseState) {
+      case "pending":
+        return pendingProps;
+      case "fulfilled":
+        return fulfilledProps;
+      case "rejected":
+        return rejectedProps;
+      default:
+        return {};
+    }
+  };
+
+  render() {
+    const handler =
+      this.state.promiseState === "pending" ? undefined : this.actionHandler;
+    return this.props.children(handler, this.asyncProps());
+  }
+}
+
+export default AsyncPropProvider;
